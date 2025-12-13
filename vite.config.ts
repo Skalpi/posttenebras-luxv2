6d3ea5f9b0d5c018fb6fadcfe0a90642533f71d6
Skalpi <skalpigoesforum@gmx.de>
2025-12-13 20:09:29 +0100

    fix: Adjust font paths and base URL for deployment
    Updates font paths in CSS and the `base` property in `vite.config.ts` to correctly reference assets when the application is deployed in a subdirectory (e.g., `/prophetien/`). This ensures that fonts are loaded correctly on the server.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  // WICHTIG: Der Pfad muss dem Ordner auf dem Server entsprechen
+  base: '/prophetien/', 
 });
